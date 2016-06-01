398ae8c601c7b03cad795a230683dab54fd346b8
ivan boo <ivanblade@outlook.com>
2016-06-01 23:39:23 +0300

    preparing examples project

---
+++ HelperMethods/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace HelperMethods.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Fruits = new string[] { "Apple", "Orange", "Pear" };
+            ViewBag.Cities = new string[] { "New York", "London", "Paris" };
+            string message = "This is an HTML element: <input>";
+            return View((object)message);
+        }
+    }
+}
