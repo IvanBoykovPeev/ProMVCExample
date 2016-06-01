11372568bc1063bc92167f1bb4e42b80d3f0cda5
ivan boo <ivanblade@outlook.com>
2016-06-02 00:18:24 +0300

    add external helper method

---
+++ HelperMethods/Infrastructure/CustumHelpers.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+
+namespace HelperMethods.Infrastructure
+{
+    public static class CustumHelpers
+    {
+        public static MvcHtmlString ListArrayItems(this HtmlHelper html, string[] list)
+        {
+            TagBuilder tag = new TagBuilder("ul");
+
+            foreach (string str in list)
+            {
+                TagBuilder itemTag = new TagBuilder("li");
+                itemTag.SetInnerText(str);
+                tag.InnerHtml += itemTag.ToString();
+            }
+
+            return new MvcHtmlString(tag.ToString());
+        }
+    }
+}
